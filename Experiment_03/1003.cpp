79b7796cda5c452709bc8c63f7ce8dc337773aee
zemt9815 <zemtation@live.com>
2018-11-28 20:49:40 +0800

    ？？

--- Experiment_03/1003.cpp
+++ Experiment_03/1003.cpp
@@ -31,41 +31,28 @@ void ClearTree(BiThrNode *tree){
     }
 }
 
-bool CompareTree(BiThrNode *t0, BiThrNode *t1){
-    if(t0&&t1){
-        if(t0->data!=t1->data){
-            return false;
-        }
-        else{
-            return (
-                CompareTree(t0->lchild, t1->lchild)
-                &&
-                CompareTree(t0->rchild, t1->rchild)
-            );
-        }
-    }
-    else if(!t0&&!t1){
-        return true;
-    }
-    else{
-        return false;
+void SwichTree(BiThrTree tree){
+
+}
+
+void PrintTree(BiThrTree tree){
+    if(tree){
+        cout<<tree->data;
+        PrintTree(tree->lchild);
+        PrintTree(tree->rchild);
     }
 }
 
 int main(){
-    char str0[100], str1[100];
-    BiThrNode *tree0, *tree1;
+    char str0[100];
+    BiThrNode *tree0;
     while(1){
         cin>>str0;
         if(!strcmp(str0, "0"))  break;
-        cin>>str1;
         ii=0;
         CreatTree(tree0, str0);
-        ii=0;
-        CreatTree(tree1, str1);
-        if(CompareTree(tree0, tree1))   cout<<"YES"<<endl;
-        else    cout<<"NO"<<endl;
+        SwichTree(tree0);
+        PrintTree(tree0);
         ClearTree(tree0);
-        ClearTree(tree1);
     }
 }
